408e976e3c18431616d52d698b1f670892ed05c3
jluizmonte <joseluiz@fedora>
2022-12-12 13:06:32 -0300

    correção campo de data da matrícula

--- src/com/controlecci/view/SCCI/AlunoView.java
+++ src/com/controlecci/view/SCCI/AlunoView.java
@@ -731,6 +731,11 @@ public class AlunoView extends javax.swing.JInternalFrame {
         jLabel34.setText("CARGA HORÁRIA");
 
         jtfDataMatriculaAlterar.setEditable(false);
+        try {
+            jtfDataMatriculaAlterar.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
         jtfDataMatriculaAlterar.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         jtfDataMatriculaAlterar.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
 
@@ -1037,7 +1042,7 @@ public class AlunoView extends javax.swing.JInternalFrame {
         jPanel6Layout.setHorizontalGroup(
             jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jLabel36, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 627, Short.MAX_VALUE)
+            .addComponent(jScrollPane1)
             .addComponent(jLabel37, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel6Layout.createSequentialGroup()
                 .addContainerGap()
@@ -1342,7 +1347,7 @@ public class AlunoView extends javax.swing.JInternalFrame {
         alunoModel.setCpf(jtfCpf.getText());
         alunoModel.setCurso(jtfCurso.getText().toUpperCase());
         alunoModel.setDataNascimento(jtfData.getText());
-        alunoModel.setDataMatricula(getDateUtil.getDateTimeUS());
+        alunoModel.setDataMatricula(jtfDataMatricula.getText());
         alunoModel.setEmail(jtfEmail.getText().toUpperCase());
         alunoModel.setEndereco(jtfEndereco.getText().toUpperCase());
         alunoModel.setNome(jtfNome.getText().toUpperCase());
@@ -1383,6 +1388,16 @@ public class AlunoView extends javax.swing.JInternalFrame {
 
     private void alterarAluno() {
         alunoModel = new AlunoModel();
+
+        String opcao1 = JOptionPane.showInputDialog(this, "O aluno possui alguma pendência?\n 1. Sim\n 2. Não", "2");
+        if (opcao1.equals("1")) {
+            String pendencia = JOptionPane.showInputDialog(this, "Insira a pendência do aluno!");
+            alunoModel.setPendencia(pendencia);
+            salvarAluno();
+        } else {
+            alunoModel.setPendencia("SEM PENDÊNCIAS");
+        }
+
         alunoModel.setMatricula(Integer.valueOf(jtfMatriculaAlterar.getText()));
         alunoModel.setBairro(jtfBairroAlterar.getText().toUpperCase());
         alunoModel.setCelular(jtfCelularAlterar.getText());
